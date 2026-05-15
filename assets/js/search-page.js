9e1ddb89d6c7ca40032bf26449a2d9d653b76a65
Rong <joy40217.26@gmail.com>
2026-05-15 10:02:25 +0800

    Update search pages
    更新並精簡搜尋頁面的顯示

--- assets/js/search-page.js
+++ assets/js/search-page.js
@@ -1,50 +1,94 @@
-const params =
-  new URLSearchParams(window.location.search);
+const params =  new URLSearchParams(window.location.search);
 
 const query = params.get('q');
 
-document.querySelector('h3').innerText =
-  `Search Results for "${query}"`;
-
 fetch('/assets/data/search.json')
-  .then(res => res.json())
-  .then(data => {
+    .then(res => res.json())
+    .then(data => {
 
-    const fuse = new Fuse(data, {
-      keys: [
-        'title',
-        'description',
-        'category',
-        'hashtag',
-        'tag'
-      ],
-      threshold: 0.3
-    });
+        const container = document.getElementById('results');
 
-    const results = fuse.search(query);
 
-    const container = document.getElementById('results');
-    const none = document.getElementById('none');
+        // 沒有搜尋詞
+        if(!query){
+            document.querySelector('h3').innerText = ``;
+            container.innerHTML =
+                data.map(item => `
+                    <article><a href="${item.url}" class="link">
+                        <span class="image">
+                            <img src="${item.img}" alt="" />
+                        </span>
+                        <header class="major">
+                            <h3>${item.title}</h3>
+                            <p>${item.description}</p>
+                            <a class="icon fa-solid fa-hashtag"></a>${item.hashtag}<br />
+                            <a class="icon fa-solid fa-tag"></a>${item.tag}
+                        </header>
+                    </a></article>
 
-    if(results.length === 0){
-      none.innerHTML =`
-        <h5>No results found.</h5>`;
-      return;
-    }
+                `).join('');
 
-    container.innerHTML =
-      results.map(result => `
-        <article><a href="${result.item.url}" class="link">
-            <span class="image">
-                <img src="${result.item.img}" alt="" />
-            </span>
-            <header class="major">
-                <h3>${result.item.title}</h3>
-                <p>${result.item.description}</p>
-                <a class="icon fa-solid fa-hashtag"></a>${result.item.hashtag}<br />
-                <a class="icon fa-solid fa-tag"></a>${result.item.tag}
-            </header>
-        </a></article>
+            return;
+        }
 
-      `).join('');
-  });
+        // 有搜尋詞
+        const fuse = new Fuse(data, {
+            keys: [
+                'title',
+                'description',
+                'category',
+                'hashtag',
+                'tag'
+            ],
+            threshold: 0.3
+        });
+
+        const results = fuse.search(query);
+
+        const none = document.getElementById('none');
+
+        document.querySelector('h3').innerText = `Search Results for "${query}"`;
+
+        if(results.length === 0){
+            none.innerHTML =`
+                <h5>
+                    No exact matches.<br />
+                    Explore all content below.
+                </h5>
+                `;
+            
+            container.innerHTML =
+                data.map(item => `
+                    <article><a href="${item.url}" class="link">
+                        <span class="image">
+                            <img src="${item.img}" alt="" />
+                        </span>
+                        <header class="major">
+                            <h3>${item.title}</h3>
+                            <p>${item.description}</p>
+                            <a class="icon fa-solid fa-hashtag"></a>${item.hashtag}<br />
+                            <a class="icon fa-solid fa-tag"></a>${item.tag}
+                        </header>
+                    </a></article>
+
+                `).join('');
+
+            return;
+        } else {
+            container.innerHTML =
+                results.map(result => `
+                    <article><a href="${result.item.url}" class="link">
+                        <span class="image">
+                            <img src="${result.item.img}" alt="" />
+                        </span>
+                        <header class="major">
+                            <h3>${result.item.title}</h3>
+                            <p>${result.item.description}</p>
+                            <a class="icon fa-solid fa-hashtag"></a>${result.item.hashtag}<br />
+                            <a class="icon fa-solid fa-tag"></a>${result.item.tag}
+                        </header>
+                    </a></article>
+
+            `).join('');
+        }
+});
